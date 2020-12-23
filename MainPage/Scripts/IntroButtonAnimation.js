13b93f00a82187ae68dd021913e7e764e2eaea59
Denis <69825463+DenchickPenchick@users.noreply.github.com>
2020-12-23 10:52:06 +0300

    New button animation

--- MainPage/Scripts/IntroButtonAnimation.js
+++ MainPage/Scripts/IntroButtonAnimation.js
@@ -1,24 +1,26 @@
 //Войтенко Денис 9А
-//Данный скрипт отвечает за анимацию первой кнопки на странице
 let introButton = document.querySelector('.get-tour-button');
 let img = introButton.querySelector('.intro-arrow');
 
-introButton.onmouseout = function() {    
+introButton.onmouseleave = function() {    
     introButton.style.transition = 'all 0.5s';
-    introButton.style.backgroundColor = '#feca30';    
+    introButton.style.background = '#feca30';    
+    introButton.style.boxShadow = 'none';
 }
 
-introButton.onmouseover = function() {    
+introButton.onmouseenter = function() {    
     introButton.style.transition = 'all 0.5s';
-    introButton.style.backgroundColor = '#daa300';        
+    introButton.style.backgroundColor = 'white';        
+    introButton.style.boxShadow = '0 0 10px rgb(255, 255, 255)';
 }
 
 introButton.onmouseup = function() {
     introButton.style.transition = 'all 0.5s';
-    introButton.style.backgroundColor = '#daa300';       
+    introButton.style.backgroundColor = 'white';       
+    introButton.style.boxShadow = '0 0 10px rgb(255, 255, 255)';
 }
 
 introButton.onmousedown = function() {
     introButton.style.transition = 'all 0.5s';
-    introButton.style.backgroundColor = '#feca30';    
+    introButton.style.boxShadow = '0 0 20px rgb(255, 255, 255)';
 }
